28567503a6c76d18b7f574f0ae0d0794c57a85b6
Joel Garcia <Joel.garcia1202@hotmail.com>
2025-02-15 16:31:10 +0100

    Backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+# Backend
+from flask import Flask, request
+import jwt
 
 app = Flask(__name__)
 
-app.run()
+
+@app.route("/")
+def index():
+    return {"Backend": "Server"}
+
+@app.route("/queue")
+def queue():
+    return {"Queue": "Route"}
+
+app.run(debug=True)
